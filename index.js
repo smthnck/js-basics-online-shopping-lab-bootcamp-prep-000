79cf1ad511dcacb891cde7e13b9d3a9f299f98a1
Nick Smith <smthnck@gmail.com>
2018-10-02 01:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  if(cart){
+  if(cart.length>0){
     for(let i =0;i<cart.length;i++){
       if (cart[i].itemName === item){
         removed = cart.splice(i,1)
