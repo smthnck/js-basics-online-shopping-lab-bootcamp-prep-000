b58d650ee895e37547e13d6a20961ec4b82ffb0f
Nick Smith <smthnck@gmail.com>
2018-10-02 01:42:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,9 +57,6 @@ function removeFromCart(item) {
         return 'That item is not in your cart.'
       }
     }
-  } else {
-      return 'That item is not in your cart.'
-  }
 }
 
 function placeOrder(cardNumber) {
