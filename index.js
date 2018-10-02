b5c3333b8ebadb8aba4c703333da16d2b7c26059
Nick Smith <smthnck@gmail.com>
2018-10-02 01:43:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
         removed = cart.splice(i,1)
         return cart
       } else {
-        return 'That item is not in your cart.'
+          return 'That item is not in your cart.'
       }
     }
 }
