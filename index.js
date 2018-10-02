7529c8a9cd3a0e754b6d43dfbba547e06e56dbee
Nick Smith <smthnck@gmail.com>
2018-10-02 02:03:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   var total = 0
-  if (!carnumber){
+  if (!cardNumber){
     return 'Sorry, we don'+'t have a credit card on file'
   } else {
     for (let i = 0;i<cart.length;i++){
