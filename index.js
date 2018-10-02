6c83be27844f00af8fd7bc64226b17be0005ae8d
Nick Smith <smthnck@gmail.com>
2018-10-02 02:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ function placeOrder(cardNumber) {
       total=+cart[i].itemPrice
       removeFromCart(cart[i].itemPrice)
     }
-  } return `Your total cost is $${total}, which will be charged to the card $.`
+  } return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   
 }
